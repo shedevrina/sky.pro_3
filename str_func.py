439c6e334846892071ea7f57cf339dd70226fa0e
shedevrina <shevrina88@gmail.com>
2023-04-04 01:42:51 +0700

    Добавлена вторая функция.

--- str_func.py
+++ str_func.py
@@ -4,13 +4,9 @@ def register_first_symbol(string):
     new_string = string.title()
     return new_string
 
-def summ_all(num_1, num_2):
-    """Сумма двух чисел. Докстринг нужно дописать."""
-    summ = num_1 + num_2
-    return summ
-
-def summ_all(num_1, num_2):
-    """Сумма двух чисел. На вход подаются числа.""
-    summ = num_1 + num_2
-    return summ
+def register_first_symbol(string):
+    """Делает заглавными первые буквы каждого слова в строке, поступившей на вход функции."""
+    string = str()
+    new_string = string.title()
+    return new_string
 
