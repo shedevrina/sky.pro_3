6bfcdeeb5413ef630628a2a19462486b20dabbd8
shedevrina <90352291+shedevrina@users.noreply.github.com>
2023-04-04 01:22:05 +0700

    Update str_func.py
    Удаление функции register.

--- str_func.py
+++ str_func.py
@@ -1,12 +1,11 @@
-﻿def register(string):
-    """Задача №3. Изменения в develop. Принимает на вход строку и возвращает ее со всеми заглавными буквами."""
-    string = str()
-    caps_string = string.upper()
-    return caps_string
-
 def register_first_symbol(string):
     """Делает заглавными первые буквы каждого слова в строке, поступившей на вход функции."""
     string = str()
     new_string = string.title()
     return new_string
+
+def summ_all(num_1, num_2):
+    """Сумма двух чисел. Докстринг нужно дописать."""
+    summ = num_1 + num_2
+    return summ
 
