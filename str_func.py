2d959a3cec7bd437c22ff20192e8ae65a14be924
shedevrina <90352291+shedevrina@users.noreply.github.com>
2023-04-04 01:48:42 +0700

    Добавлена вторая функция. (#7)
    Выбран вариант локального изменения файла str_func.py

--- str_func.py
+++ str_func.py
@@ -3,3 +3,10 @@ def register_first_symbol(string):
     string = str()
     new_string = string.title()
     return new_string
+
+def register_first_symbol(string):
+    """Делает заглавными первые буквы каждого слова в строке, поступившей на вход функции."""
+    string = str()
+    new_string = string.title()
+    return new_string
+
